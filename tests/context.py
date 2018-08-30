739c1291292876874e78ae1d401362fc361278c2
Junior Teudjio <jun.teudjio@gmail.com>
2018-08-30 07:36:06 +0100

    configure testing environment context

--- tests/context.py
+++ tests/context.py
@@ -5,3 +5,15 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import runtimedocs
+
+
+try:
+    # try block to test if in python 3
+    from unittest import mock
+    import builtins
+    builtin_str = 'builtins'
+except ImportError:
+    # here in python 2
+    import mock
+    builtin_str = '__builtin__'
+
