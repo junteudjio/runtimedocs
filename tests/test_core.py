10cb96511eb3bc640641389db27839782fda8a95
Junior Teudjio <jun.teudjio@gmail.com>
2018-09-02 13:37:34 +0100

    add test_multi_return_output

--- tests/test_core.py
+++ tests/test_core.py
@@ -107,5 +107,25 @@ def test_add_extra_handler(mock_open, mock_getLogger, func):
     assert mock_getLogger.call_count == 1
     assert mock_getLogger().addHandler.call_count == 2 + len(dummy_handlers)
 
+@pytest.mark.parametrize('func_return_value', ['bla', ('bla', 'foo')])
+@mock.patch('runtimedocs.core.logging.getLogger', autospec=True)
+@mock.patch('{builtin}.open'.format(builtin=builtin_str))
+def test_multi_return_output(mock_open, mock_getLogger, func_return_value, func):
+    # arrange
+    func.return_value = func_return_value
+    decorated_func = runtimedocs.core.runtimedocs()(func)
+
+    # call
+    decorated_func()
+
+    # assert
+    assert mock_getLogger.call_count == 1
+    if isinstance(func_return_value, tuple):
+        mock_getLogger().\
+           info.assert_has_calls([mock.call('returned value is a tuple and could be a multi output return statement:')])
+    else:
+        mock_getLogger(). \
+            info.assert_has_calls(
+            [mock.call('single output return statement:')])
 
 
